974d462a2f6f88d96e93efbe81b4a351eedd895e
Your Name <you@example.com>
2024-12-13 08:42:42 +0300

    add rhombus

---
+++ GeometryLibrary/src/main/java/com/geometry/shapes/Rhombus.java
@@ -0,0 +1,21 @@
+package com.geometry.shapes;
+
+public class Rhombus implements Shape {
+    private final double diagonal1;
+    private final double diagonal2;
+
+    public Rhombus(double diagonal1, double diagonal2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+    }
+
+    @Override
+    public double getArea() {
+        return diagonal1 * diagonal2 / 2;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return diagonal1 * 2 + diagonal2 * 2;
+    }
+}
